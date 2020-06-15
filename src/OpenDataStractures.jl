c50217cfc52873a81eada4a1db1bdab62c1030a8
Twovillage <stickhatpursesandals@gmail.com>
2020-06-15 23:31:18 +0900

    separate test code

--- src/OpenDataStractures.jl
+++ src/OpenDataStractures.jl
@@ -1,17 +1,3 @@
 module OpenDataStractures
 
-include("ArrayStack.jl")
-using .ArrayStack
-
-a = BaseArray{Int}(0)
-for i = 1:10
-    add!(a, i, i)
-end
-
-remove!(a, 5)
-
-for i = 1:a.n
-    println(a.array[i])
-end
-
 end # module
